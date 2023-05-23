f6a101e1aa186f1101430a4dee405ab86a1014f0
AbednegoChristianyoel <abednegoefra31@gmail.com>
2023-05-23 21:43:02 +0700

    Add bottom nav, scan activity, and CameraX
    Add buttom nav, home fragment, profile fragment, scan activity, dan CameraX

---
+++ Mobile-Development/Mendaur/app/src/main/java/com/dicoding/abednego/mendaur/ui/scan/ScanActivity.kt
@@ -0,0 +1,169 @@
+package com.dicoding.abednego.mendaur.ui.scan
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.location.Location
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.CompoundButton
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.dicoding.abednego.mendaur.R
+import com.dicoding.abednego.mendaur.databinding.ActivityScanBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.File
+
+class ScanActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityScanBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var getFile: File? = null
+    private var currentLocation: Location? = null
+    private var positionLat: RequestBody? = null
+    private var positionLon: RequestBody? = null
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.no_permission),
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityScanBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = getString(R.string.title_upload_story)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        getMyLocation()
+
+        binding.switchAddLocation.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            if (isChecked) {
+                binding.switchAddLocation.text = getString(R.string.location_enabled)
+                val location = currentLocation
+                if (location != null) {
+                    positionLat = location.latitude.toString().toRequestBody("text/plain".toMediaType())
+                    positionLon = location.longitude.toString().toRequestBody("text/plain".toMediaType())
+                }
+            } else {
+                binding.switchAddLocation.text = getString(R.string.location_disabled)
+            }
+        }
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+
+        binding.btnCamerax.setOnClickListener { startCameraX() }
+        binding.btnGallery.setOnClickListener { startGallery() }
+
+    }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                if (location != null) {
+                    currentLocation = location
+                }
+            }
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+    }
+
+    private fun startCameraX() {
+        val intent = Intent(this, CameraActivity::class.java)
+        launcherIntentCameraX.launch(intent)
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = getString(R.string.intent_type)
+        val chooser = Intent.createChooser(intent, getString(R.string.choose_image))
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentCameraX = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == CAMERA_X_RESULT) {
+            val myFile = it.data?.getSerializableExtra(getString(R.string.picture)) as File
+            val isBackCamera = it.data?.getBooleanExtra(getString(R.string.is_back_camera), true) as Boolean
+
+            val resultFile = reduceFileImageAndroidX(myFile, isBackCamera)
+            binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(resultFile.path))
+            getFile = resultFile
+        }
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+            val myFile = uriToFile(selectedImg, this@ScanActivity)
+            getFile = myFile
+            binding.previewImageView.setImageURI(selectedImg)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val CAMERA_X_RESULT = 200
+
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+}
