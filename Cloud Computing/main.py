b249aac6cf6239e427d9dded18d433938360c70e
And Than <92675586+Bam280@users.noreply.github.com>
2023-06-14 23:40:41 +0700

    Update Final API

--- Cloud Computing/main.py
+++ Cloud Computing/main.py
@@ -1,4 +1,6 @@
 import json
+import asyncio
+import smtplib
 from os import getcwd, path
 import uvicorn
 import tensorflow as tf
@@ -15,11 +17,11 @@ from pydantic import BaseModel
 from PIL import Image
 
 app = FastAPI()
-project_id = "your_project_id"
-key_path = path.join(getcwd(), "path/to/serviceAccount.json")
+project_id = "mendaur-projtest"
+key_path = path.join(getcwd(), "service-account.json")
 credentials = service_account.Credentials.from_service_account_file(key_path)
 db = firestore.Client(project=project_id, credentials=credentials)
-CLOUD_STORAGE_BUCKET = "your_bucket"
+CLOUD_STORAGE_BUCKET = "mendaur-projtest.appspot.com"
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 class Article(BaseModel):
@@ -30,6 +32,8 @@ class Article(BaseModel):
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+
+app = FastAPI()
 RECYCLE_METHOD_FILE = path.join(getcwd(), path.join("dataset", path.join("recycle_recommendation.json")))
 MODEL_PATH = path.join(getcwd(), path.join("model", path.join("model_fv_resnet50_v6.h5")))
 MODEL = tf.keras.models.load_model(MODEL_PATH, custom_objects={'KerasLayer':hub.KerasLayer})
@@ -71,7 +75,7 @@ def home():
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
     if not allowed_file(file.filename):
-        return JSONResponse(content="Jenis file tidak valid. Silakan unggah gambar.", status_code=400)
+        return JSONResponse(content="Invalid file type. Please upload an image.", status_code=400)
 
     image = Image.open(BytesIO(await file.read()))
     image_arr = np.asarray(image)
@@ -84,74 +88,83 @@ async def post_articles(
     content: str = Form(...),
     file: UploadFile = File(...),
 ):
-    try:
-        gcs = storage.Client()
-        bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)
-        file_path = f"articles_img/{file.filename}"
-        blob = bucket.blob(file_path)
 
+    gcs = storage.Client()
+    bucket = gcs.get_bucket(CLOUD_STORAGE_BUCKET)  
+    file_path = f"articles_img/{file.filename}"
+    blob = bucket.blob(file_path)
+
+    blob.upload_from_file(file.file)
+    img_url = blob.public_url
+    
+    doc_test = db.collection('users').document(uid)
+    data = doc_test.get()
+    if data.exists:
+        datauser=data.to_dict()
+        name=datauser['name']
+        photo_url=datauser['photourl']
+
+    doc_ref = db.collection('articles').document()
+    doc_ref.set({
+        'name': name,
+        'profile_picture': photo_url,
+        'title': title,
+        'content': content,
+        'url': img_url,
+        'createdAt': datetime.datetime.now().isoformat() + "Z"
+    })
+
+    return {"status": "success", "public url": img_url}
+
+@app.get("/get-articles")
+async def get_articles():
+    articles = []
+    articles_ref = db.collection('articles').stream()
+
+    for doc in articles_ref:
+        article_data = doc.to_dict()
+        article_data["createdAt"] = datetime.datetime.strptime(article_data["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat()
+        articles.append(article_data)
+
+    return articles
+
+@app.post('/reports')
+async def create_report(
+    uid: str = Form(...),
+    title: str = Form(...),
+    content: str = Form(...),
+    file: UploadFile = File(...),
+):
+    try:
+        # Upload file to Cloud Storage
+        file_path = f"reports_img/{file.filename}"
+        blob = bucket.blob(file_path)
         blob.upload_from_file(file.file)
         img_url = blob.public_url
 
+        # Retrieve user data from Firestore
         doc_test = db.collection('users').document(uid)
         data = doc_test.get()
         if data.exists:
             datauser = data.to_dict()
             name = datauser['name']
-            photo_url = datauser['photourl']
+        else:
+            return {'status': 'User not found'}, 404
 
-        doc_ref = db.collection('articles').document()
+        # Create a new report in Firestore
+        doc_ref = db.collection('reports').document()
         doc_ref.set({
             'name': name,
-            'profile_picture': photo_url,
             'title': title,
             'content': content,
             'url': img_url,
-            'createdAt': datetime.datetime.now().isoformat() + "Z"
+            'createdAt': datetime.datetime.now()
         })
 
-        res_dict = {
-            "error": False,
-            "message": "Data berhasil diunggah",
-            "public url": img_url
-        }
-        return res_dict
-
-    except Exception as err:
-        res_dict = {
-            "error": True,
-            "message": "Terjadi kesalahan saat memposting artikel.",
-            "details": str(err)
-        }
-        return JSONResponse(content=res_dict, status_code=500)
-
-@app.get("/get-articles")
-async def get_articles():
-    try:
-        articles = []
-        articles_ref = db.collection('articles').stream()
-
-        for doc in articles_ref:
-            article_data = doc.to_dict()
-            article_data["createdAt"] = datetime.datetime.strptime(article_data["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ").isoformat()
-            articles.append(article_data)
-
-        res_dict = {
-            "error": False,
-            "message": "Berhasil mengambil data",
-            "articles": articles
-        }
-        return res_dict
-
-    except Exception as err:
-        res_dict = {
-            "error": True,
-            "message": "Terjadi kesalahan saat mengambil artikel.",
-            "details": str(err)
-        }
-        return JSONResponse(content=res_dict, status_code=500)
-
-
+        return {'status': 'Report created successfully'}
+    except Exception as e:
+        print('Error creating report:', str(e))
+        return {'status': 'Error creating report'}, 500
 
 @app.get("/get-recycle")
 async def recycle_rec(jenis: Union[str, None] = None):
@@ -176,7 +189,6 @@ async def recycle_rec(jenis: Union[str, None] = None):
         res_dict["error"] = True
         res_dict["message"] = "Internal Server Error"
         return JSONResponse(content=res_dict, status_code=500)
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8080)
